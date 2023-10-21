2ddbf3380b3df03c42f0c9010d811df8e777ded2
SHREYASH DUBEY <shreyashdubey@SHREYASHs-MacBook-Pro.local>
2023-10-21 10:57:56 +0530

    Firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'home_page.dart';
@@ -6,7 +7,16 @@ const Color primaryColor = Color(0xFFFF4A57); // Primary color
 const Color bgColor = Color(0x242639); // Background color
 const Color cardColor = Color(0x2B2D40); // Card color
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyC4GZL4SIRaEleVQ82SX4a1C7BD3aO3dNM",
+      appId: "1:873674021768:web:a589383610642543c6783d",
+      messagingSenderId: "873674021768",
+      projectId: "portfolioweb-6bda1",
+    ),
+  );
   runApp(const MyApp());
 }
 
